e2d5c1d9c2934d3498360fc13115baf5f4719883
esraanggraini <105895881+esraanggraini@users.noreply.github.com>
2022-05-20 12:19:34 +0700

    first upload
    menjadi lebih baik

---
+++ database.js
@@ -0,0 +1,62 @@
+let menu=[
+    {
+        kode:"brg001",
+        nama:"jus mangga",
+        harga:"10000",
+        foto:"https://img-global.cpcdn.com/recipes/539561beae7b8af3/680x482cq70/jus-mangga-kental-segar-foto-resep-utama.jpg"
+    }, {
+        kode:"brg002",
+        nama:"nasi goreng",
+        harga:"15000",
+        foto:"https://cdn1-production-images-kly.akamaized.net/KxuztQKl3tnUN0Fw5iAwKsnX_u0=/0x148:1920x1230/640x360/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3093328/original/069244600_1585909700-fried-2509089_1920.jpg"
+    },
+    {
+        kode:"brg003",
+        nama:"mie goreng",
+        harga:"18000",
+        foto:"https://cdn0-production-images-kly.akamaized.net/ZbmIfg2wSl77g2-IjkJpgPONAOM=/0x342:5989x3718/673x373/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3930827/original/066100900_1644554867-shutterstock_1984582070.jpg"
+    },
+    {
+        kode:"brg004",
+        nama:"bicap",
+        harga:"20000",
+        foto:"https://img-global.cpcdn.com/recipes/f38ddaf1df11ef81/680x482cq70/pgba-4-bicap-babi-kecap-foto-resep-utama.jpg"
+    },
+    {
+        kode:"brg005",
+        nama:"es teh",
+        harga:"5000",
+        foto:"https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2021/06/15093247/Ketahui-Fakta-Es-Teh-Manis.jpg"
+    },
+    {
+        kode:"brg006",
+        nama:"ice crem",
+        harga:"8000",
+        foto:"https://cdn1-production-images-kly.akamaized.net/G1uaVEdGYxfa43Z3kvaIYp8GUGI=/341x142:1647x1448/1200x1200/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3104602/original/048837600_1587095459-shutterstock_201774374.jpg"
+    },
+    {
+        kode:"brg007",
+        nama:"bakwan",
+        harga:"25000",
+        foto:"https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/02/06/598927952.jpg"
+    },
+    {
+        kode:"brg008",
+        nama:"ayam bakar",
+        harga:"30000",
+        foto: "https://i0.wp.com/resepkoki.id/wp-content/uploads/2017/11/Resep-Ayam-bakar-bumbu-bali.jpg?fit=1742%2C1887&ssl=1"
+    },
+    {
+        kode:"brg009",
+        nama:"ayam goreng",
+        harga:"35000",
+        foto:"https://selerasa.com/wp-content/uploads/2015/12/images_daging_ayam-goreng-1200x720.jpg"
+    },
+    {
+        kode:"brg0010",
+        nama:"sate",
+        harga:"40000",
+        foto:"https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/10/12/460178680.jpg"
+    },
+]
+console.log(menu)
